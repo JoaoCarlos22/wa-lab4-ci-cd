0cce7f7874c2ea2d898f51909c96736b1eff7c79
JoaoCarlos22 <jcarlosguimaraes13@gmail.com>
2026-08-13 15:34:46 -0400

    :tada: init: inicalizacao do projeto

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Bem vindos ao WebAcademy!!');
+});
+
+app.listen(8082, () => {
+  console.log('Rodando na porta 8082');
+});
